0425dfdc65ce5690b1e0816d8db53ae2b4a91f9b
Eugene Petryankin <epetryankin@devvela.com>
2021-04-14 18:30:41 +0300

    API route was moved to .env

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 export const fetchParticipants = async url => {
-  const { data } = await axios.post('http://localhost:5000/api/participants', {
-    url,
-  });
+  const { data } = await axios.post(
+    import.meta.env.VITE_API_ROUTE + '/api/participants',
+    {
+      url,
+    }
+  );
 
   return data;
 };
